310953be2d48d28e7a9013084feb8b99d8e7b0f2
Dmitry <deus.main@gmail.com>
2023-09-18 13:53:50 +0300

    🚀feat(response) add debug prints

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def index():
     if secret is None:
         return "Access Denied (the secret key is not set)"
     if _is_signature_correct(body, secret, signature):
+        print("✅ signature_correct")
+        print("⌛ preparing response")
         return _prepare_response(body.decode("utf-8"))
     return "Access Denied"
 
@@ -34,10 +36,12 @@ def _prepare_response(body):
     comment_author = comment["author"]
     author_name = comment_author["first_name"] + " " + comment_author["last_name"]
     comment_text = "Hello, {}! You said: {}".format(author_name, comment["text"])
+    print("✅ response is ready")
     return '{{ "text":"{}", "reassign_to":{{ "id":{} }} }}'.format(
         comment_text, comment_author["id"]
     )
 
 
 if __name__ == "__main__":
+    print("✅ server is ready")
     app.run(host="0.0.0.0", port=port)

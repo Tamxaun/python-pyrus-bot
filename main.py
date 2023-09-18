8f87c52a2d04208d8f1386fb64dd856b0100a996
Dmitry <deus.main@gmail.com>
2023-09-18 13:19:22 +0300

    🛠️refactor(main) update config handling

--- main.py
+++ main.py
@@ -5,37 +5,40 @@ import json
 from flask import Flask
 from flask import request
 
-
-app = Flask(__name__)
-app.config.from_file('config.json', load=json.load)
-port = int(os.environ.get('PORT', 5000))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-@app.route('/', methods=['GET', 'POST'])
+app = Flask(__name__)
+app.config.from_file("config.json", load=json.load)
+port = int(os.environ.get("PORT", 5000))
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-   body = request.data
-   signature = request.headers['x-pyrus-sig']
-   secret = str.encode(SECRET_KEY)
-   if secret is None:
-      return "Access Denied (the secret key is not set)"
-   if _is_signature_correct(body, secret, signature):
-      return _prepare_response(body.decode('utf-8'))
-   return "Access Denied"
+    body = request.data
+    signature = request.headers["x-pyrus-sig"]
+    secret = str.encode(SECRET_KEY)
+    if secret is None:
+        return "Access Denied (the secret key is not set)"
+    if _is_signature_correct(body, secret, signature):
+        return _prepare_response(body.decode("utf-8"))
+    return "Access Denied"
 
 
 def _is_signature_correct(message, secret, signature):
-   digest = hmac.new(secret, msg=message, digestmod=hashlib.sha1).hexdigest()
-   return hmac.compare_digest(digest, signature.lower())
+    digest = hmac.new(secret, msg=message, digestmod=hashlib.sha1).hexdigest()
+    return hmac.compare_digest(digest, signature.lower())
 
 
 def _prepare_response(body):
-   task = json.loads(body)["task"]
-   comment = task["comments"][-1]
-   comment_author = comment["author"]
-   author_name = comment_author["first_name"] + " " + comment_author["last_name"]
-   comment_text = "Hello, {}! You said: {}".format(author_name, comment["text"])
-   return "{{ \"text\":\"{}\", \"reassign_to\":{{ \"id\":{} }} }}".format(comment_text, comment_author["id"])
+    task = json.loads(body)["task"]
+    comment = task["comments"][-1]
+    comment_author = comment["author"]
+    author_name = comment_author["first_name"] + " " + comment_author["last_name"]
+    comment_text = "Hello, {}! You said: {}".format(author_name, comment["text"])
+    return '{{ "text":"{}", "reassign_to":{{ "id":{} }} }}'.format(
+        comment_text, comment_author["id"]
+    )
 
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port)
+    app.run(host="0.0.0.0", port=port)

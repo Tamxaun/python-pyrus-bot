b0f0f1f32b6af87096b3a8e43dded58146b833bf
Dmitry <deus.main@gmail.com>
2024-02-04 17:58:21 +0300

    🚀feat(tests) add unit test file

---
+++ tests/test_unittest.py
@@ -0,0 +1,17 @@
+# from main import pyrus_api
+import unittest
+
+
+class Test_TestFormatFields(unittest.TestCase):
+    def test_fields(self):
+        # forms_test = pyrus_api.get_request("https://api.pyrus.com/v4/forms/1346484")
+        # task_test = pyrus_api.get_request("https://api.pyrus.com/v4/tasks/1346484")
+
+        # print(forms_test)
+        # print(task_test)
+
+        self.assertEqual(True, True)
+
+
+if __name__ == "__main__":
+    unittest.main()

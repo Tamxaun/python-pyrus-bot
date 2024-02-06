1b2ec72b4ca98eaa1bb46b3f64919df3b9f1f17b
Dmitry <deus.main@gmail.com>
2024-02-06 18:56:38 +0300

    🛠️refactor(reminder_payment_type) refactor validation logic 🛠️refactor(reminder_payment_type) refactor response preparation ⚙️chore(reminder_payment_type) update comment formatting ⚙️chore(reminder_payment_type) update signature check ⚙️chore(reminder_payment_type) handle different request bodies 🚀feat(reminder_payment_type) process request and prepare response

--- bot/reminder_payment_type.py
+++ bot/reminder_payment_type.py
@@ -23,12 +23,13 @@ class ReminderPaymentType:
             print("⛔ The request does not have the X-Pyrus-Sig.")
             print(self.request.headers)
             return False
-        # check if body and secret is set
-        if self.pyrus_secret_key is None or self.body is None:
-            print(f"Body is {'set ✅' if self.body != None else 'not set ❌'}")
-            print(
-                f"Secret is {'set ✅' if self.pyrus_secret_key != None else 'not set ❌'}"
-            )
+        # check if secret is set
+        if self.pyrus_secret_key is None or not self.pyrus_secret_key:
+            print("Secret is not set ❌", self.pyrus_secret_key)
+            return False
+        # check if body is set
+        if self.body is None or not self.body:
+            print("Body is not set ❌")
             return False
 
         # chech signature
@@ -36,46 +37,52 @@ class ReminderPaymentType:
         digest = hmac.new(secret, msg=self.body, digestmod=hashlib.sha1).hexdigest()
         return hmac.compare_digest(digest, self.signature.lower())
 
-    def _prepare_response(self):
+    def _prepare_response(self, task: dict):
         print("⌛ Preparing response")
 
-        # task = json.loads(self.request.data)["task"]
+        person = f"<a href='https://pyrus.com/t#pp486746'>Татьяна Ивановна</a>"
+        text = "Для данного заказа требуется оформить:<br><ul><li>приходный кассовый ордер</li><li>оформить подотчет на Бусырев А.А.</li><ul>"
+        comment_text = "{person}<br>{text}".format(person=person, text=text)
 
-        # print("👋 task", task)
+        hasUpdatedFields = (
+            "comments" in task and "task_field_updates" in task["comments"]
+        )
 
-        # person = f"<a href='https://pyrus.com/t#pp486746'>Татьяна Ивановна</a>"
-        # text = "Для данного заказа требуется оформить:<br><ul><li>приходный кассовый ордер</li><li>оформить подотчет на Бусырев А.А.</li><ul>"
-        # comment_text = "{person}<br>{text}".format(person=person, text=text)
+        if hasUpdatedFields:
+            task_field_updates = task["comments"]["task_field_updates"]
 
-        # hasUpdatedFields = (
-        #     "comments" in task and "task_field_updates" in task["comments"]
-        # )
+            for field in task_field_updates:
+                isPaymenType = (
+                    "name" in field and field["name"] == "Тип оплаты / Статус"
+                )
+                isCorrectPaymenType = (
+                    "value" in field
+                    and isinstance(field["value"], dict)
+                    and "choice_names" in field["value"]
+                    and field["value"]["choice_names"][0] == "✅Нал (чек)"
+                )
 
-        # if hasUpdatedFields:
-        #     task_field_updates = task["comments"]["task_field_updates"]
+                if isPaymenType and isCorrectPaymenType:
+                    return ('{{ "formatted_text":"{}" }}'.format(comment_text), 200)
 
-        #     for field in task_field_updates:
-        #         isPaymenType = (
-        #             "name" in field and field["name"] == "Тип оплаты / Статус"
-        #         )
-        #         isCorrectPaymenType = (
-        #             "value" in field
-        #             and isinstance(field["value"], dict)
-        #             and "choice_names" in field["value"]
-        #             and field["value"]["choice_names"][0] == "✅Нал (чек)"
-        #         )
-
-        #         if isPaymenType and isCorrectPaymenType:
-        #             return ('{{ "formatted_text":"{}" }}'.format(comment_text), 200)
-
-        # return "{}", 200
-
-        task = json.loads(self.body.decode("utf-8"))["task"]
-        return '{{"text": {} }}'.format(task)
+        return "{}", 200
 
     def process_request(self):
         if not self._validate_request():
+            print("❌ Signature is not correct")
             return "🚫 Access Denied", 403
 
         print("✅ Signature_correct")
-        return self._prepare_response()
+
+        try:
+            data = json.loads(self.body.decode("utf-8"))
+            if "task" in data:
+                print("😉 Body contains 'task'")
+                task = data["task"]
+                return self._prepare_response(task)
+            else:
+                print("😢 Body does not contain 'task'")
+                return "🚫 Access Denied", 403
+        except json.JSONDecodeError:
+            print("😢 Body is not valid JSON")
+            return "🚫 Access Denied", 403

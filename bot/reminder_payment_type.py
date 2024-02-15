068f98647740a60b00b52a2359a8a506e470a706
Dmitry <deus.main@gmail.com>
2024-02-15 14:28:50 +0300

    🛠️refactor(bot) modify response in ReminderPaymentType
    Explanation:
    - Refactored the response message in the ReminderPaymentType class in the bot module.
    - Modified the formatting and content of the response message.
    - Replaced the yellow highlighting with bold tags.
    - Added instructions to assign a specific status after completing the required actions.

--- bot/reminder_payment_type.py
+++ bot/reminder_payment_type.py
@@ -40,9 +40,9 @@ class ReminderPaymentType:
     def _prepare_response(self, task: dict):
         print("⌛ Preparing response")
 
-        person = f"<a href='https://pyrus.com/t#pp486746'><mark data-color='yellow'>Татьяна Ивановна</mark></a>"
-        text = f"Для данного заказа требуется оформить:<ul><li>Приходный кассовый ордер<br></li><li>Оформить подотчет на Бусырев А.А.<br></li></ul>"
-        comment_text = f"{person}<br>{text}"
+        person = f"<a href='https://pyrus.com/t#pp486746'>Татьяна Ивановна</a>"
+        text = f"Для данного заказа требуется оформить:<ul><li>Приходный кассовый ордер</li><li>Оформить подотчет на Бусырев А.А.</li></ul><br>После чего назначить статус '✅Нал (чек принят в 1С)' в поле 'Тип оплаты / Статус'"
+        comment_text = f"<mark data-color='yellow'>{person}</mark><br>{text}<br>"
         subscribers_added = [
             {
                 "id": 486746,

195cde3d4f240e1f9c5a893ff64d0ee51f5c094a
Dmitry <deus.main@gmail.com>
2024-02-21 16:47:10 +0300

    🚀feat(api) enhance API authentication and request handling 🛠️refactor(api) improve error handling and response processing

--- pyrus_api_handler.py
+++ pyrus_api_handler.py
@@ -12,24 +12,30 @@ class PyrusAPI:
         self.pyrus_login = pyrus_login
         self.pyrus_secret_key = pyrus_secret_key
 
-    def _auth(self):
+    def _auth(self) -> str:
         print("⌛ Starting API authentication request to Pyrus")
 
-        auth_data = requests.get(
-            "https://api.pyrus.com/v4/auth",
-            params={"login": self.pyrus_login, "security_key": self.pyrus_secret_key},
-        ).text
+        try:
+            r = requests.get(
+                "https://api.pyrus.com/v4/auth",
+                params={
+                    "login": self.pyrus_login,
+                    "security_key": self.pyrus_secret_key,
+                },
+            )
+            r.raise_for_status()  # This line raises an HTTPError if the HTTP request returned an unsuccessful status code
+            auth_data: str = json.loads(r.text)
+        except requests.exceptions.RequestException as e:
+            # Handle any request exceptions
+            print(f"❌ API AUTH: Failed to get authentication token from Pyrus: {e}")
+            raise Exception(e)
 
         token = json.loads(auth_data)["access_token"]
+        print(f"✅ API AUTH: Success to get authentication token from Pyrus")
 
-        if token is None:
-            print("❌ Failed to get authentication token from Pyrus")
-            return None
-
-        print("✅ Success to get authentication token from Pyrus")
         return token
 
-    def get_request(self, url: str) -> Optional[dict]:
+    def get_request(self, url: str) -> dict:
         print("⌛ API GET: Making a  request...")
 
         if self.token is None:
@@ -37,11 +43,6 @@ class PyrusAPI:
                 "⚠️ API GET: Authentication token is not in cache, creating a new one..."
             )
             self.token = self._auth()
-            if self.token is None:
-                print(
-                    "⚠️ API GET: Authentication token is not ready from _auth() method"
-                )
-                return None
             print("🫙 API GET: Saving authentication token in cache...")
             self.cache.set("pyrus_auth_token", self.token)
         else:
@@ -52,22 +53,28 @@ class PyrusAPI:
             "Content-Type": "application/json",
         }
 
-        r = requests.get(url, headers=headers)
-        r_data: dict = json.loads(r.text)
-
-        if r.status_code == 200:
-            print("✅ API GET: Response is ready", r.status_code)
+        try:
+            r = requests.get(url, headers=headers)
+            r.raise_for_status()  # This line raises an HTTPError if the HTTP request returned an unsuccessful status code
+            r_data: dict = json.loads(r.text)
+            # Process the response data
             return r_data
-        elif r.status_code == 401:
-            print("⚠️ API GET: Authentication token is expired")
-            self.token = self._auth()
-            self.cache.set("pyrus_auth_token", self.token)
-            return self.get_request(url)
-        else:
-            print("⚠️ API GET Response is not ready", r.status_code)
-            return None
+        except requests.exceptions.HTTPError as err:
+            # Handle 401 Unauthorized error
+            if r.status_code == 401:
+                self.token = self._auth()
+                self.cache.set("pyrus_auth_token", self.token)
+                return self.get_request(url)
+            else:
+                # Handle other HTTP errors
+                print(f"⚠️ API GET: HTTP error occurred: {err}")
+                raise Exception(err)
+        except requests.exceptions.RequestException as e:
+            # Handle any request exceptions
+            print(f"⚠️ API GET: An error occurred: {e}")
+            raise Exception(e)
 
-    def post_request(self, url, data):
+    def post_request(self, url, data) -> dict:
         print("⌛ API POST: Making request...")
 
         if self.token is None:
@@ -75,16 +82,8 @@ class PyrusAPI:
                 "⚠️ API POST: Authentication token is not in cache, creating a new one..."
             )
             self.token = self._auth()
-
-            if self.token is None:
-                print(
-                    "⚠️ API POST: Authentication token is not ready from .auth() method"
-                )
-                return None
-
             print("🫙 API POST: Saving authentication token in cache...")
             self.cache.set("pyrus_auth_token", self.token)
-
         else:
             print("🫙 API GET: Authentication token in cache")
 
@@ -93,17 +92,23 @@ class PyrusAPI:
             "Content-Type": "application/json",
         }
 
-        r = requests.post(url=url, data=data, headers=headers)
-        r_data = r.json()
-
-        if r.status_code == 200:
-            print("✅ API POST: Response is ready", r.status_code)
+        try:
+            r = requests.post(url=url, json=data, headers=headers)
+            r.raise_for_status()  # This line raises an HTTPError if the HTTP request returned an unsuccessful status code
+            r_data = r.json()
+            # Process the response data
             return r_data
-        elif r.status_code == 401:
-            print("⚠️ API POST: Authentication token is expired")
-            self.token = self._auth()
-            self.cache.set("pyrus_auth_token", self.token)
-            return self.post_request(url, data)
-        else:
-            print("⚠️ API POST: Response is not ready", r.status_code)
-            return None
+        except requests.exceptions.HTTPError as err:
+            # Handle 401 Unauthorized error
+            if r.status_code == 401:
+                self.token = self._auth()
+                self.cache.set("pyrus_auth_token", self.token)
+                return self.post_request(url, data)
+            else:
+                # Handle other HTTP errors
+                print(f"⚠️ API POST: HTTP error occurred: {err}")
+                raise Exception(err)
+        except requests.exceptions.RequestException as e:
+            # Handle any request exceptions
+            print(f"⚠️ API POST: An error occurred: {e}")
+            raise Exception(e)

2acadae00d4f3fde18cfb6aa9f25a4a976ba1979
Dmitry <deus.main@gmail.com>
2024-03-14 18:39:35 +0300

    build(deps): update main.py and requirements.txt

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-from flask import request
-from flask_caching import Cache
-from flask_apscheduler import APScheduler
-from pyrus_api_handler import PyrusAPI
-from bot.reminder_step import ReminderStep
-from bot.reminder_payment_type import ReminderPaymentType
-from bot.remider_inactive_tasks import RemiderInactiveTasks
-from bot.create_notify_date import CreateNotificationDate
-from notify_in_pyrus_task import Notification_in_pyrus_task
-import sentry_sdk
+
+# from flask import request
+# from flask_caching import Cache
+# from flask_apscheduler import APScheduler
+# from pyrus_api_handler import PyrusAPI
+# from bot.reminder_step import ReminderStep
+# from bot.reminder_payment_type import ReminderPaymentType
+# from bot.remider_inactive_tasks import RemiderInactiveTasks
+# from bot.create_notify_date import CreateNotificationDate
+# from notify_in_pyrus_task import Notification_in_pyrus_task
+# import sentry_sdk
 
 
 # Initialize the Flask app
@@ -28,16 +29,16 @@ else:
 # locale.setlocale(locale.LC_TIME)
 
 # Initialize Sentry
-sentry_sdk.init(
-    dsn="https://4cc58ab824b087258eac2255dbfd9e99@o1295012.ingest.sentry.io/4506705877860352",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-)
+# sentry_sdk.init(
+#     dsn="https://4cc58ab824b087258eac2255dbfd9e99@o1295012.ingest.sentry.io/4506705877860352",
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     traces_sample_rate=1.0,
+#     # Set profiles_sample_rate to 1.0 to profile 100%
+#     # of sampled transactions.
+#     # We recommend adjusting this value in production.
+#     profiles_sample_rate=1.0,
+# )
 
 # Load environment variables
 LOGIN = os.getenv("LOGIN")
@@ -68,16 +69,16 @@ else:
     print("✅ All required environment variables are set")
 
 # Configure the Flask app
-config = {"DEBUG": False, "CACHE_TYPE": "SimpleCache", "CACHE_DEFAULT_TIMEOUT": 300}
-app.config.from_mapping(config)
+# config = {"DEBUG": False, "CACHE_TYPE": "SimpleCache", "CACHE_DEFAULT_TIMEOUT": 300}
+# app.config.from_mapping(config)
 port = int(os.environ.get("PORT", 5000))
 
 
 # Initialize the cache
-cache = Cache(app)
+# cache = Cache(app)
 
 # Initialize the Pyrus API
-pyrus_api = PyrusAPI(cache, RS_LOGIN, RS_SECRET_KEY)
+# pyrus_api = PyrusAPI(cache, RS_LOGIN, RS_SECRET_KEY)
 
 
 @app.route("/", methods=["GET"])
@@ -107,67 +108,67 @@ def index_page():
     return "✅ Server is ready"
 
 
-@app.route("/step-reminder", methods=["GET", "POST"])
-def reminder_step_page():
-    reminder_step_page = ReminderStep(
-        cache=cache,
-        request=request,
-        pyrus_secret_key=RS_SECRET_KEY,
-        pyrus_login=RS_LOGIN,
-    )
-    return reminder_step_page.process_request()
+# @app.route("/step-reminder", methods=["GET", "POST"])
+# def reminder_step_page():
+#     reminder_step_page = ReminderStep(
+#         cache=cache,
+#         request=request,
+#         pyrus_secret_key=RS_SECRET_KEY,
+#         pyrus_login=RS_LOGIN,
+#     )
+#     return reminder_step_page.process_request()
 
 
-@app.route("/reminder-payment-type", methods=["GET", "POST"])
-def reminder_peyment_type_page():
-    reminder_peyment_type = ReminderPaymentType(
-        cache=cache,
-        request=request,
-        pyrus_secret_key=RPT_SECRET_KEY,
-        pyrus_login=RPT_LOGIN,
-    )
-    return reminder_peyment_type.process_request()
+# @app.route("/reminder-payment-type", methods=["GET", "POST"])
+# def reminder_peyment_type_page():
+#     reminder_peyment_type = ReminderPaymentType(
+#         cache=cache,
+#         request=request,
+#         pyrus_secret_key=RPT_SECRET_KEY,
+#         pyrus_login=RPT_LOGIN,
+#     )
+#     return reminder_peyment_type.process_request()
 
 
-@app.route("/remider-inactive-tasks", methods=["GET", "POST"])
-def remider_inactive_tasks_page():
-    remider_inactive_tasks = RemiderInactiveTasks(
-        cache=cache,
-        request=request,
-        pyrus_secret_key=RIT_SECRET_KEY,
-        pyrus_login=RIT_LOGIN,
-        sentry_sdk=sentry_sdk,
-    )
-    return remider_inactive_tasks.process_request()
+# @app.route("/remider-inactive-tasks", methods=["GET", "POST"])
+# def remider_inactive_tasks_page():
+#     remider_inactive_tasks = RemiderInactiveTasks(
+#         cache=cache,
+#         request=request,
+#         pyrus_secret_key=RIT_SECRET_KEY,
+#         pyrus_login=RIT_LOGIN,
+#         sentry_sdk=sentry_sdk,
+#     )
+#     return remider_inactive_tasks.process_request()
 
 
-@app.route("/create-notification-date", methods=["GET", "POST"])
-def notify_date_shipment_page():
-    catalog_id = "211552"
-    create_notification_date = CreateNotificationDate(
-        catalog_id,
-        cache,
-        NDS_SECRET_KEY,
-        NDS_LOGIN,
-        sentry_sdk,
-    )
-    return create_notification_date.process_request(request)
+# @app.route("/create-notification-date", methods=["GET", "POST"])
+# def notify_date_shipment_page():
+#     catalog_id = "211552"
+#     create_notification_date = CreateNotificationDate(
+#         catalog_id,
+#         cache,
+#         NDS_SECRET_KEY,
+#         NDS_LOGIN,
+#         sentry_sdk,
+#     )
+#     return create_notification_date.process_request(request)
 
 
-# initialize scheduler
-scheduler = APScheduler()
-scheduler.api_enabled = True
-scheduler.init_app(app)
-scheduler.start()
+# # initialize scheduler
+# scheduler = APScheduler()
+# scheduler.api_enabled = True
+# scheduler.init_app(app)
+# scheduler.start()
 
 
-@scheduler.task("cron", id="notify_job", hour=8, minute=5, timezone="Europe/Moscow")
-def notify_job():
-    catalog_id = "211552"
-    notification = Notification_in_pyrus_task(
-        catalog_id, NDS_LOGIN, NDS_SECRET_KEY, sentry_sdk, cache
-    )
-    notification.send()
+# @scheduler.task("cron", id="notify_job", hour=8, minute=5, timezone="Europe/Moscow")
+# def notify_job():
+#     catalog_id = "211552"
+#     notification = Notification_in_pyrus_task(
+#         catalog_id, NDS_LOGIN, NDS_SECRET_KEY, sentry_sdk, cache
+#     )
+#     notification.send()
 
 
 if __name__ == "__main__":

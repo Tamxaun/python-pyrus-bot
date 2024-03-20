214f8621d412b2fa7ac565fa908382bd7143f6bc
Dmitry <deus.main@gmail.com>
2024-03-20 20:34:10 +0300

    🚀feat(routes) update index_page response format

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-import os
 from flask import Flask
 
 # Initialize the Flask app
 app = Flask(__name__)
 
-# Configure the Flask app
-config = {"DEBUG": False}
-app.config.from_mapping(config)
-port = int(os.environ.get("PORT", 5000))
 
-
-@app.route("/", methods=["GET"])
+@app.route("/")
 def index_page():
-    return "✅ Server is ready"
+    return "<p>✅ Server is ready</p>"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
     print("✅ Server is ready")

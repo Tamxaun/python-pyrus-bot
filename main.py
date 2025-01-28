8569b5d4297072a5a16e265678979c0294d19802
Dmitry <deus.main@gmail.com>
2025-01-28 16:25:38 +0300

    🛠️refactor(main) set server port to a fixed value of 5000 and remove environment variable fallback

--- main.py
+++ main.py
@@ -186,7 +186,8 @@ def notify_job():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    # port = int(os.environ.get("PORT", 5000))
+    port = 5000
     app.run(
         debug=False,
         use_reloader=False,

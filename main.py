96a4e21ad9da2e8f11c8df32acd6dd3b7af46b3d
Dmitry <deus.main@gmail.com>
2025-01-28 17:56:08 +0300

    🚀feat(main) load environment variables from .env file and update server run configuration

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from flask import Flask
 import os
+from flask import Flask
+from dotenv import load_dotenv, find_dotenv
+
+dotenv_path = find_dotenv()
+if dotenv_path:
+    load_dotenv(dotenv_path)
+    print("✅ Loaded environment variables from .env file")
+else:
+    print("⚠️ .env file not found, using server environment variables")
+    print(
+        "check some server environment variables, DEFAULT_PORT:",
+        os.getenv("DEFAULT_PORT"),
+    )
 
 app = Flask(__name__)
 
@@ -11,4 +23,4 @@ def index_page():
 
 if __name__ == "__main__":
     port = 8080
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)

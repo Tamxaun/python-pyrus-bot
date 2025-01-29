71c62699cf73d7db444e1745af87097c2dd60808
Dmitry <deus.main@gmail.com>
2025-01-29 12:38:08 +0300

    🛠️refactor(main) clean up commented-out code and improve webhook structure

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ from bot.reminder_step import ReminderStep
 
 import sentry_sdk
 
-# from bot.reminder_payment_type import ReminderPaymentType
 from bot.sync_task_data import SyncTaskData
 
-# from bot.remider_inactive_tasks import RemiderInactiveTasks
 from bot.create_reminder_comment import CreateReminderComment, TrackedFieldsType
 from notify_in_pyrus_task import Notification_in_pyrus_task
 
@@ -97,28 +95,6 @@ pyrus_api = PyrusAPI(
 
 @app.route("/", methods=["GET"])
 def index_page():
-    # data = pyrus_api.get_request("https://api.pyrus.com/v4/tasks/201302847")
-    # if data is not None:
-    #     task = data["task"]
-    #     task_fields = task["fields"]
-    #     print("🍿task_fields", task_fields)
-
-    # person = f"<a href='https://pyrus.com/t#pp486746'>Татьяна Ивановна</a>"
-    # text = "Для данного заказа требуется оформить:<br><ul><li>приходный кассовый ордер</li><li>оформить подотчет на Бусырев А.А.</li><ul>"
-    # comment_text = "{person}<br>{text}".format(person=person, text=text)
-
-    # for field in task_fields:
-    #     isPaymenType = "name" in field and field["name"] == "Тип оплаты / Статус"
-    #     isCorrectPaymenType = (
-    #         "value" in field
-    #         and isinstance(field["value"], dict)
-    #         and "choice_names" in field["value"]
-    #         and field["value"]["choice_names"][0] == "✅Нал (чек)"
-    #     )
-
-    #     if isPaymenType and isCorrectPaymenType:
-    #         return ('{{ "formatted_text":"{}" }}'.format(comment_text), 200)
-
     return "✅ Server is ready"
 
 
@@ -133,60 +109,39 @@ def reminder_step_page():
     return reminder_step_page.process_request()
 
 
-# @app.route("/reminder-payment-type", methods=["GET", "POST"])
-# def reminder_peyment_type_page():
-#     reminder_peyment_type = ReminderPaymentType(
-#         CACHE,
-#         request,
-#         RPT_SECRET_KEY if RPT_SECRET_KEY is not None else "",
-#         RPT_LOGIN if RPT_LOGIN is not None else "",
-#     )
-#     return reminder_peyment_type.process_request()
-
-
-# @app.route("/remider-inactive-tasks", methods=["GET", "POST"])
-# def remider_inactive_tasks_page():
-#     remider_inactive_tasks = RemiderInactiveTasks(
-#         cache=cache,
-#         request=request,
-#         pyrus_secret_key=RIT_SECRET_KEY,
-#         pyrus_login=RIT_LOGIN,
+# @app.route("/webhook-sync-task-data", methods=["GET", "POST"])
+# def webhook_sync_task_data():
+#     TRACKED_FIELD = {
+#         "Заказ в Pyrus": ["№ ордеров из 1С", "№ ордера"],
+#     }
+#     sync_task_data = SyncTaskData(
+#         cache=CACHE,
+#         pyrus_secret_key=SYNC_SECRET_KEY if SYNC_SECRET_KEY is not None else "",
+#         pyrus_login=SYNC_LOGIN if SYNC_LOGIN is not None else "",
 #         sentry_sdk=sentry_sdk,
+#         traked_fields=TRACKED_FIELD,
 #     )
-#     return remider_inactive_tasks.process_request()
-@app.route("/webhook-sync-task-data", methods=["GET", "POST"])
-def webhook_sync_task_data():
-    TRACKED_FIELD = {
-        "Заказ в Pyrus": ["№ ордеров из 1С", "№ ордера"],
-    }
-    sync_task_data = SyncTaskData(
-        cache=CACHE,
-        pyrus_secret_key=SYNC_SECRET_KEY if SYNC_SECRET_KEY is not None else "",
-        pyrus_login=SYNC_LOGIN if SYNC_LOGIN is not None else "",
-        sentry_sdk=sentry_sdk,
-        traked_fields=TRACKED_FIELD,
-    )
-    return sync_task_data.process_request(request=request)
+#     return sync_task_data.process_request(request=request)
 
 
-@app.route(rule="/webhook-reminder", methods=["GET", "POST"])
-def webhook_reminder():
-    CATALOG_ID = "211552"
-    TRACKED_FIELDS: TrackedFieldsType = {
-        "text": {
-            "Тип оплаты / Статус": "✅Нал (чек)",
-        },
-        "date": ["Дата отгрузки", "Дата планируемой оплаты"],
-    }
-    create_reminder_comment = CreateReminderComment(
-        CATALOG_ID,
-        CACHE,
-        REMINDER_SECRET_KEY if REMINDER_SECRET_KEY is not None else "",
-        REMINDER_LOGIN if REMINDER_LOGIN is not None else "",
-        sentry_sdk,
-        TRACKED_FIELDS,
-    )
-    return create_reminder_comment.process_request(request)
+# @app.route(rule="/webhook-reminder", methods=["GET", "POST"])
+# def webhook_reminder():
+#     CATALOG_ID = "211552"
+#     TRACKED_FIELDS: TrackedFieldsType = {
+#         "text": {
+#             "Тип оплаты / Статус": "✅Нал (чек)",
+#         },
+#         "date": ["Дата отгрузки", "Дата планируемой оплаты"],
+#     }
+#     create_reminder_comment = CreateReminderComment(
+#         CATALOG_ID,
+#         CACHE,
+#         REMINDER_SECRET_KEY if REMINDER_SECRET_KEY is not None else "",
+#         REMINDER_LOGIN if REMINDER_LOGIN is not None else "",
+#         sentry_sdk,
+#         TRACKED_FIELDS,
+#     )
+#     return create_reminder_comment.process_request(request)
 
 
 # @scheduler.task("cron", id="notify_job", hour=8, minute=5, timezone="Europe/Moscow")
